9db8e44236d4cbcd8ba777476cc505ce3f3e8b37
parthasarathy venkataraman <pvenka@parthasarathys-MacBook-Pro.local>
2016-10-21 16:06:49 +0530

    test3

--- demapp/views.py
+++ demapp/views.py
@@ -17,6 +17,8 @@ def gitIssues(request):
 	times = []
 	alldict = {}
 	alldat = []
+	errdict = {}
+	errs = []
         global issue
 	if request.method == 'POST':
 		git_rep = str(request.POST.get('gits'))
@@ -24,30 +26,32 @@ def gitIssues(request):
 		url = 'https://api.github.com/repos/' + git_rep +'/'+git_repo+'/issues?'
 		qstr = urllib.urlencode({'status':'open'})
 		url = url+qstr
+		
 		req = requests.get(url)
-		r = req.text
-		issue = json.loads(r)
-		today = datetime.datetime.today()
-		for item in issue:
-        		diff = today-datetime.datetime.strptime(item["created_at"],'%Y-%m-%dT%H:%M:%SZ')
-        		times.append((diff.days))
-		ltday = 0
-		ltweek = 0
-		mtweek = 0
-		for i in times:
-        		if i < 1:
-          			ltday = ltday +1
-        		elif (i >= 1 and i <7):
-          			ltweek = ltweek + 1
-        		else:
-          			mtweek = mtweek +1
-	        #alldat.append(ltday)
-		#alldat.append(ltweek)
-		#alldat.append(mtweek)
-		alldict["less_than_a_day"] = ltday
-		alldict["less_than_a_week"] = ltweek
-		alldict["more_than_a_week"] = mtweek
-		alldat.append(alldict)
-	return render(request,'demapp/gitIssues.html', {'data':alldat})
+		if req.status_code==200:
+			r = req.text
+               		issue = json.loads(r)
+                	today = datetime.datetime.today()
+                	for item in issue:
+                        	diff = today-datetime.datetime.strptime(item["created_at"],'%Y-%m-%dT%H:%M:%SZ')
+                        	times.append((diff.days))
+                	ltday = 0
+                	ltweek = 0
+                	mtweek = 0
+                	for i in times:
+                        	if i < 1:
+                                	ltday = ltday +1
+                        	elif (i >= 1 and i <7):
+                                	ltweek = ltweek + 1
+                        	else:
+                                	mtweek = mtweek +1
+                	alldict["less_than_a_day"] = ltday
+                	alldict["less_than_a_week"] = ltweek
+                	alldict["more_than_a_week"] = mtweek
+                	alldat.append(alldict)
+       	      #		return render(request,'demapp/gitIssues.html', {'data':alldat})
+		else:
+			errdict['err'] = req.status_code
+			errs.append(errdict)
 
-	
+	return render(request,'demapp/gitIssues.html', {'data':alldat,'err':errs})
